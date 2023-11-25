879c8f734c62913602cd9347a5a030ca31491236
WarterPL <Warter@github.com>
2023-11-25 22:47:27 +0100

    QUICKFIX

---
+++ G#/WConsole.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WClasses
+{
+    public class WConsole
+    {
+        public static string ReadString(string text = "")
+        { Console.Write(text); return Console.ReadLine(); }
+        public static float ReadFloat(string text = "")
+        { Console.Write(text); return float.Parse(Console.ReadLine()); }
+        public static double ReadDouble(string text = "")
+        { Console.Write(text); return double.Parse(Console.ReadLine()); }
+        public static int ReadInt(string text = "")
+        { Console.Write(text); return int.Parse(Console.ReadLine()); }
+        
+        public static void Write(string text, string end="\n") => Console.Write(text + end);
+
+        public static void Recolor(ConsoleColor foreground = ConsoleColor.White, ConsoleColor background = ConsoleColor.Black)
+        { Console.ForegroundColor = foreground; Console.BackgroundColor = background; }
+    }
+}
--- Testing/Program.cs
+++ Testing/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WClasses;
 
 namespace Testing
 {
@@ -10,6 +11,10 @@ namespace Testing
     {
         static void Main(string[] args)
         {
+            string showme = WConsole.ReadString("nword? : ");
+            Console.WriteLine(showme);
+
+            Console.ReadLine();
         }
     }
 }
