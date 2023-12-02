88133815f4c93f03962a96f4bcf221ed6f3d0622
WarterPL <Warter@github.com>
2023-12-02 20:39:43 +0100

    5ty wymiar

--- G#/W.cs
+++ G#/W.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Linq;
-using System.Runtime.InteropServices.Expando;
-using WClasses.Helpers;
 
 namespace WClasses
 {
@@ -27,96 +25,5 @@ namespace WClasses
             avg /= array.Length;
             return avg;
         }
-
-        public static void ArrayPrinter<T>(T[] array, string expand = null)
-        {
-            Console.ForegroundColor = ConsoleColor.DarkGray;
-
-            for (int i = 0; i < array.Length; i++)
-            {
-                bool lastRow = i == array.GetLength(0) - 1;
-                var isLastRow = lastRow ? "└── " : "├── ";
-                var rowDisplay = i == 0 ? (expand == null ? "┌── " : "├── ") : isLastRow;
-                Console.Write($"{expand}{rowDisplay}");
-
-                Console.ForegroundColor = ConsoleColor.DarkCyan;
-                    Console.WriteLine(array[i]);
-                Console.ForegroundColor = ConsoleColor.DarkGray;
-            }
-            if(expand == null)
-                Console.ResetColor();
-        }
-        public static void Array2DPrinter<T>(T[,] array,string expand = null)
-        {
-            Console.ForegroundColor = ConsoleColor.DarkGray;
-            for (int i = 0; i < array.GetLength(0); i++)
-            {
-                bool lastRow = i == array.GetLength(0) - 1;
-                var isLastRow = lastRow ? "└── " : "├── ";
-                var rowDisplay = i == 0 ? (expand == null ? "┌── " : "├── ") : isLastRow;
-                Console.Write($"{expand}{rowDisplay}");
-
-                Console.ForegroundColor = ConsoleColor.DarkGreen;
-                    Console.WriteLine($"Row number {i}");
-                Console.ForegroundColor = ConsoleColor.DarkGray;
-
-                var expander = i == array.GetLength(0) - 1 ? "    " : "│   ";
-                var _1Dim = array.Extract(i);
-                ArrayPrinter(_1Dim, expand + expander);
-            }
-            if (expand == null)
-                Console.ResetColor();
-        }
-        public static void Array3DPrinter<T>(T[,,] array, string expand = null)
-        {
-            for(int i = 0; i < array.GetLength(0); i++)
-            {
-                Console.ForegroundColor = ConsoleColor.DarkGray;
-                bool lastRow = i == array.GetLength(0) - 1;
-                var isLastRow = lastRow ? "└── " : "├── ";
-                var rowDisplay = i == 0 ? (expand == null ? "┌── " : "├── ") : isLastRow;
-                Console.Write($"{expand}{rowDisplay}");
-
-                Console.ForegroundColor = ConsoleColor.DarkYellow;
-                    Console.WriteLine($"Thickness {i}");
-                Console.ForegroundColor = ConsoleColor.DarkGray;
-
-                var _2Dim = array.Extract(i);
-
-                var expander = i == array.GetLength(0) - 1 ? "    " : "│   ";
-                Array2DPrinter(_2Dim, expand + expander);
-            }
-            if(expand == null)
-                Console.ResetColor();
-        }
-        public static void Array4DPrinter<T>(T[,,,] array, string expand = null)
-        {
-            for (int i = 0; i < array.GetLength(0); i++)
-            {
-                Console.ForegroundColor = ConsoleColor.DarkGray;
-                bool lastRow = i == array.GetLength(0) - 1;
-                var isLastRow = lastRow ? "└── " : "├── ";
-                var rowDisplay = i == 0 ? (expand == null ? "┌── " : "├── ") : isLastRow;
-                Console.Write($"{expand}{rowDisplay}");
-
-                Console.ForegroundColor = ConsoleColor.DarkRed;
-                Console.WriteLine($"4th Dimension layer {i}");
-                Console.ForegroundColor = ConsoleColor.DarkGray;
-
-                var _3Dim = array.Extract(i);
-
-                var expander = i == array.GetLength(0) - 1 ? "    " : "│   ";
-                Array3DPrinter(_3Dim, expand + expander);
-            }
-            if (expand == null)
-                Console.ResetColor();
-        }
-    }
-    public enum ArrayOperation
-    {
-        None = 0,
-        Reverse = 1,
-        Sorted = 2,
-        SortedAndReversed = 3
     }
 }
---
+++ G#/WArray.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WClasses
+{
+    public static class WArray
+    {
+        public static T[] Extract<T>(this T[,] array, int index)
+        {
+            int _1stDimLength = array.GetLength(1);
+            T[] output = new T[_1stDimLength];
+
+            for (int i = 0; i < _1stDimLength; i++)
+            {
+                output[i] = array[index, i];
+            }
+
+            return output;
+        }
+        public static T[,] Extract<T>(this T[,,] array, int index)
+        {
+            int _1stDimLength = array.GetLength(1);
+            int _2ndDimLength = array.GetLength(2);
+            T[,] output = new T[_1stDimLength, _2ndDimLength];
+
+            for (int i = 0; i < _1stDimLength; i++)
+            {
+                for (int j = 0; j < _2ndDimLength; j++)
+                {
+                    output[i, j] = array[index, i, j];
+                }
+            }
+            return output;
+        }
+        public static T[,,] Extract<T>(this T[,,,] array, int index)
+        {
+            int _1stDimLength = array.GetLength(1);
+            int _2ndDimLength = array.GetLength(2);
+            int _3ndDimLength = array.GetLength(3);
+            T[,,] output = new T[_1stDimLength, _2ndDimLength, _3ndDimLength];
+
+            for (int i = 0; i < _1stDimLength; i++)
+            {
+                for (int j = 0; j < _2ndDimLength; j++)
+                {
+                    for (int k = 0; k < _3ndDimLength; k++)
+                    {
+                        output[i, j, k] = array[index, i, j, k];
+                    }
+                }
+            }
+            return output;
+        }
+        public static T[,,,] Extract<T>(this T[,,,,] array, int index)
+        {
+            int _1stDimLength = array.GetLength(1);
+            int _2ndDimLength = array.GetLength(2);
+            int _3ndDimLength = array.GetLength(3);
+            int _4thDimLength = array.GetLength(4);
+            T[,,,] output = new T[_1stDimLength, _2ndDimLength, _3ndDimLength, _4thDimLength];
+
+            for (int i = 0; i < _1stDimLength; i++)
+                for (int j = 0; j < _2ndDimLength; j++)
+                    for (int k = 0; k < _3ndDimLength; k++)
+                        for (int l = 0; l < _4thDimLength; l++)
+                            output[i, j, k, l] = array[index, i, j, k, l];
+
+            return output;
+        }
+
+        public static void Printer<T>(this T[] array, string expand = null)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                bool lastRow = i == array.GetLength(0) - 1;
+                var isLastRow = lastRow ? "└── " : "├── ";
+                var rowDisplay = i == 0 ? (expand == null ? "┌── " : "├── ") : isLastRow;
+                Console.Write($"{expand}{rowDisplay}");
+
+                Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                Console.WriteLine(array[i]);
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+            }
+            if (expand == null)
+                Console.ResetColor();
+        }
+        public static void Printer<T>(this T[,] array, string expand = null)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                bool lastRow = i == array.GetLength(0) - 1;
+                var isLastRow = lastRow ? "└── " : "├── ";
+                var rowDisplay = i == 0 ? (expand == null ? "┌── " : "├── ") : isLastRow;
+                Console.Write($"{expand}{rowDisplay}");
+
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                Console.WriteLine($"Row number {i}");
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+
+                var expander = i == array.GetLength(0) - 1 ? "    " : "│   ";
+                var _1Dim = array.Extract(i);
+                _1Dim.Printer(expand + expander);
+            }
+            if (expand == null)
+                Console.ResetColor();
+        }
+        public static void Printer<T>(this T[,,] array, string expand = null)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                bool lastRow = i == array.GetLength(0) - 1;
+                var isLastRow = lastRow ? "└── " : "├── ";
+                var rowDisplay = i == 0 ? (expand == null ? "┌── " : "├── ") : isLastRow;
+                Console.Write($"{expand}{rowDisplay}");
+
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine($"Thickness {i}");
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+
+                var _2Dim = array.Extract(i);
+
+                var expander = i == array.GetLength(0) - 1 ? "    " : "│   ";
+                _2Dim.Printer(expand + expander);
+            }
+            if (expand == null)
+                Console.ResetColor();
+        }
+        public static void Printer<T>(this T[,,,] array, string expand = null)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                bool lastRow = i == array.GetLength(0) - 1;
+                var isLastRow = lastRow ? "└── " : "├── ";
+                var rowDisplay = i == 0 ? (expand == null ? "┌── " : "├── ") : isLastRow;
+                Console.Write($"{expand}{rowDisplay}");
+
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine($"4th Dimension layer {i}");
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+
+                var _3Dim = array.Extract(i);
+
+                var expander = i == array.GetLength(0) - 1 ? "    " : "│   ";
+                _3Dim.Printer(expand + expander);
+            }
+            if (expand == null)
+                Console.ResetColor();
+        }
+        public static void Printer<T>(this T[,,,,] array, string expand = null)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                bool lastRow = i == array.GetLength(0) - 1;
+                var isLastRow = lastRow ? "└── " : "├── ";
+                var rowDisplay = i == 0 ? (expand == null ? "┌── " : "├── ") : isLastRow;
+                Console.Write($"{expand}{rowDisplay}");
+
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine($"5th Dimension layer {i}");
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+
+                var _4Dim = array.Extract(i);
+
+                var expander = i == array.GetLength(0) - 1 ? "    " : "│   ";
+                _4Dim.Printer(expand + expander);
+            }
+            if (expand == null)
+                Console.ResetColor();
+        }
+    }
+}
